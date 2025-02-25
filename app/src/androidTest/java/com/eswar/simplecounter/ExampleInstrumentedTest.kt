52f69841fc6f098cd6e0f6b9fa6a1b3b68dae82a
eswar-7116 <eswardudi06@gmail.com>
2025-02-25 20:08:27 +0530

    Add Tests

--- app/src/androidTest/java/com/eswar/simplecounter/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/eswar/simplecounter/ExampleInstrumentedTest.kt
@@ -1,24 +1,33 @@
 package com.eswar.simplecounter
 
-import androidx.test.platform.app.InstrumentationRegistry
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.performClick
 import androidx.test.ext.junit.runners.AndroidJUnit4
-
+import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 
-import org.junit.Assert.*
-
-/**
- * Instrumented test, which will execute on an Android device.
- *
- * See [testing documentation](http://d.android.com/tools/testing).
- */
 @RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
+    @get:Rule
+    val rule = createComposeRule()
+
     @Test
-    fun useAppContext() {
-        // Context of the app under test.
-        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.eswar.simplecounter", appContext.packageName)
+    fun testCounter() {
+        rule.setContent {
+            App(MainViewModel())
+        }
+
+        rule.onNodeWithText("0").assertExists()
+        rule.onNodeWithText("Increment").performClick()
+        rule.onNodeWithText("1").assertExists()
+
+        rule.onNodeWithText("Decrement").performClick()
+        rule.onNodeWithText("0").assertExists()
+
+        rule.onNodeWithText("Increment").performClick()
+        rule.onNodeWithText("Reset").performClick()
+        rule.onNodeWithText("0").assertExists()
     }
 }
